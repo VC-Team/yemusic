c069c1632ff5a9fe9f1a95f5dfe4280de105108d
Tung Anh Nguyen <tunganh2521999@gmail.com>
2022-04-03 15:13:12 +0700

    :recycle: refactor(export model User): update - export model User
    Closes #30

--- apps/server/src/datasources/models/user.ts
+++ apps/server/src/datasources/models/user.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const UserInfo = new Schema(
+const userInfo = new Schema(
   {
     firstName: {
       type: String,
@@ -22,16 +22,20 @@ const UserInfo = new Schema(
   { _id: false, id: false }
 );
 
-export const UserSchema = new Schema(
+export const userSchema = new Schema(
   {
     username: { type: String, unique: true },
     password: String,
     email: { type: String, unique: true },
     phone: { type: String, unique: true },
     desc: String,
-    info: UserInfo,
+    info: userInfo,
     isValidEmail: Boolean,
     isBlocked: Boolean,
   },
   { timestamps: true }
 );
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;

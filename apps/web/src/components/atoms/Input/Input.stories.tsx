4239c8b24402428414101b0caf85fa7d91027404
thaihoanganh <hoanganhthai161299@gmail.com>
2022-04-05 18:05:35 +0700

    :recycle: refactor(web): refactor style

--- apps/web/src/components/atoms/Input/Input.stories.tsx
+++ apps/web/src/components/atoms/Input/Input.stories.tsx
@@ -8,11 +8,26 @@ export default {
   title: 'Input',
 } as Meta;
 
-const Template: Story<InputProps> = args => <Input prefix={<HomeIcon />} suffix={<HomeIcon />} {...args} />;
+const Template: Story<InputProps> = args => <Input {...args} />;
 
 export const Preview = Template.bind({});
-
 Preview.args = {
   disabled: false,
   fullWidth: false,
+  prefix: <HomeIcon color="inherit" />,
+  suffix: <HomeIcon color="inherit" />,
+};
+
+export const Prefix = Template.bind({});
+Prefix.args = {
+  disabled: false,
+  fullWidth: false,
+  prefix: <HomeIcon color="inherit" />,
+};
+
+export const Suffix = Template.bind({});
+Suffix.args = {
+  disabled: false,
+  fullWidth: false,
+  suffix: <HomeIcon color="inherit" />,
 };

72e66d901b76bb738e9a9c70735b7495a1f1b0b0
Tung Anh Nguyen <tunganh2521999@gmail.com>
2022-04-14 14:53:28 +0700

    :recycle: refactor(implement api sendEmail): user get mail to verifyEmail
    Closes #21

---
+++ apps/server/src/datasources/utils/controllers/nodemailer/sendMailTemplate.ts
@@ -0,0 +1,22 @@
+import { Request } from 'express';
+
+import { yeToken } from '..';
+import { sendMail } from './nodemailer';
+
+export async function sendEmailVerify(req: Request, email) {
+  const tokenVerifyEmail = yeToken.createToken({ payload: { email } });
+
+  const verifyURL = `${req.protocol}://${req.get('host')}/api/auth/verifyEmail/${tokenVerifyEmail}`;
+  const info = await sendMail({
+    to: email,
+    subject: 'Verify your email to complete sign up Yemusic',
+    html: `
+      <b>Just one more step to complete the registration 🥳</b>
+      <div>
+        <a href="${verifyURL}" target="_blank">Click this link to complete</a>
+      </div>
+      `,
+  });
+
+  return { messageId: info.messageId };
+}

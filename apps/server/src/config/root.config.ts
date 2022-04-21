ac60921bcfd46c8a099c98da2b96493fe7e35123
gialynguyen <gialynguyen@gmail.com>
2022-04-21 09:19:42 +0700

    :bug: fix(bcrypt): rebuild bcrypt when build docker image

--- apps/server/src/config/root.config.ts
+++ apps/server/src/config/root.config.ts
@@ -20,7 +20,9 @@ export const youtube: Youtube = {
 
 export const mongoDB: MongoDB = {
   database_test: 'mongodb://localhost/db_yemusic_test',
-  database: process.env.YEMUSIC_DB_CONNECTION_STRING || 'mongodb://localhost/db_yemusic_test',
+  database_host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_DEFAULT || 'db_yemusic',
+  database_url: process.env.YEMUSIC_DB_CONNECTION_STRING,
   mongoOptions: {},
 };
 
--- apps/server/src/provider/mongoose.ts
+++ apps/server/src/provider/mongoose.ts
@@ -3,11 +3,15 @@ import * as mongoose from 'mongoose';
 import { mongoDB } from '../config';
 
 export const loadMongo = async () => {
-  mongoose.connect(mongoDB.database, mongoDB.mongoOptions, err => {
-    if (err) {
-      logger.info(`💩 mongodb connection failed ${err}`);
-    } else {
-      logger.info('🌈 hello from mongodb');
+  mongoose.connect(
+    mongoDB.database_url || `mongodb://${mongoDB.database_host}/${mongoDB.database}`,
+    mongoDB.mongoOptions,
+    err => {
+      if (err) {
+        logger.info(`💩 mongodb connection failed ${err}`);
+      } else {
+        logger.info('🌈 hello from mongodb');
+      }
     }
-  });
+  );
 };

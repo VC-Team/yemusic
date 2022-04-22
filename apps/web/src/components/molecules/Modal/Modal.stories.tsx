415cefe405a45a07eae4859c607facc34e8b96d1
thaihoanganh <hoanganhthai161299@gmail.com>
2022-04-22 18:15:40 +0700

    :recycle: refactor(modal): refactor modal component
    Closes #39

--- apps/web/src/components/molecules/Modal/Modal.stories.tsx
+++ apps/web/src/components/molecules/Modal/Modal.stories.tsx
@@ -7,11 +7,24 @@ export default {
   title: 'Modal',
 } as Meta;
 
-const Template: Story<ModalProps> = args => <Modal {...args} />;
+const Template: Story<ModalProps> = args => (
+  <div>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, ab quod tempora eligendi hic totam, rerum
+      non voluptatum pariatur consequatur, accusamus sapiente aliquam quisquam labore! Voluptatem unde quasi debitis
+      dignissimos?
+    </p>
+
+    <Modal {...args} />
+  </div>
+);
 
 export const Preview = Template.bind({});
 
 Preview.args = {
-  children: <div style={{ width: '20rem', height: '18rem', background: 'white', textAlign: 'center' }}>Modal</div>,
+  children: <div style={{ width: '20rem', height: '18rem', background: 'white', textAlign: 'center' }} />,
   open: true,
+  onClose: () => {
+    console.log('onClose');
+  },
 };
--- apps/web/src/components/molecules/Modal/Modal.tsx
+++ apps/web/src/components/molecules/Modal/Modal.tsx
@@ -1,20 +1,19 @@
 import React, { FC } from 'react';
 
-import classNames from 'classnames';
 import { createPortal } from 'react-dom';
 
 import './styles.scss';
 
 export interface ModalProps {
+  children?: React.ReactNode;
   open?: boolean;
   onClose?: () => void;
-  children?: React.ReactNode;
 }
 
 export const Modal: FC<ModalProps> = ({ children, open = false, onClose, ...otherProps }) => {
   const modal = (
     <div onClick={onClose} className="m-modal">
-      <div onClick={e => e.stopPropagation()} className={classNames('m-modal__sub')} {...otherProps}>
+      <div onClick={e => e.stopPropagation()} {...otherProps}>
         {children}
       </div>
     </div>

4315b573b52d87347f4d8097e53913d23302905a
Khoa Ngo <nmkhoa17ck1@gmail.com>
2022-05-02 15:21:02 +0700

    :recycle: refactor(server): Refactor unit test

--- apps/server/__tests__/common.test.ts
+++ apps/server/__tests__/common.test.ts
@@ -1,29 +1,36 @@
-import { connect, connection, disconnect } from 'mongoose';
+import * as mongoose from 'mongoose';
 import supertest = require('supertest');
 
+import '../src/global';
 import { mongoDB } from '../src/config';
 import { createServer } from '../src/provider';
 
-let status = 0;
 export const httpServer = supertest(createServer());
 
+async function startMongoose() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(mongoDB.database_test);
+    mongoose.connection
+      .once('open', () => {
+        resolve(true);
+      })
+      .on('error', error => {
+        logger.warn('Warning', error);
+        reject(error);
+      });
+  });
+}
+
 beforeAll(async () => {
-  await connect(mongoDB.database_test);
-  status = ((await connection.asPromise()) || {}).readyState;
-
-  if (status === 1) {
-    // === Drop All Database ===
-    const collections = await connection.db.collections();
-    for (const collection of collections) {
-      await collection.drop();
-    }
-  }
+  await startMongoose();
 });
 
-afterAll(done => {
-  return disconnect(done);
-});
+afterAll(async () => {
+  const collections = await mongoose.connection.db.collections();
 
-test('should Connected', async () => {
-  expect(status).toBe(1);
+  // Drop database
+  await Promise.all(collections.map(collection => collection.drop()));
+
+  // Disconnect
+  await mongoose.disconnect();
 });
---
+++ apps/server/__tests__/datasets/user/signUp.ts
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+
+export const user1 = {
+  email: faker.internet.email(),
+  password: '12313dasd',
+};
+
+export const user2 = {
+  email: faker.internet.email(),
+  password: '12313dasd',
+};

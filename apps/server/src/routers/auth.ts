02ee1c91647e7500e75d2141ce8412ac8eb55e3a
Khoa Ngo <nmkhoa17ck1@gmail.com>
2022-05-08 23:42:21 +0700

    :bug: fix(server): change method from get to pod refreshToken API

--- apps/server/src/routers/auth.ts
+++ apps/server/src/routers/auth.ts
@@ -5,7 +5,7 @@ import controller from '@controllers/user';
 const authRouter = Router();
 
 authRouter.route('/auth/signUp').post(controller.signUp);
-authRouter.route('/auth/refreshToken').get(controller.refreshToken);
+authRouter.route('/auth/refreshToken').post(controller.refreshToken);
 authRouter.route('/auth/sendEmailVerify').post(controller.sendEmailVerify);
 authRouter.route('/auth/verifyEmail/:tokenVerifyEmail').get(controller.verifyEmail);
 

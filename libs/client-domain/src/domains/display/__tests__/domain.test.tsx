43f3307d57bd26b60ead0d38c4bfbf26759ae7e9
thaihoanganh <hoanganhthai161299@gmail.com>
2022-07-17 12:56:13 +0700

    :fire: prune(lib-display-domain): remove test

--- libs/client-domain/src/domains/display/__tests__/domain.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-import { DisplayDomain } from '../domain';
-
-const { DisplayProvider, DisplayContext } = new DisplayDomain({
-  theme: 'light',
-});
